47816afa0247ba71cc90ae7aeb7f88a4e291a585
Ritish Saini <109451965+MaXiMo000@users.noreply.github.com>
2025-02-06 22:11:33 +0530

    Delete Backend directory

--- Backend/route/search.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {searchUsers} from "../controller/search.controller.js";
-const router = express.Router();
-
-// router.get("/",search);
-
-router.get('/', searchUsers);
-
-export default router;
